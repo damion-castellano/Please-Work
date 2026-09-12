23b07b3c51bdf758971543773fd99c6a8ef82c19
inner-elysium <dacastellano96@gmail.com>
2026-09-12 19:08:08 -0400

    so much
    back up in case bad stuff happens

---
+++ Scripts/start_menu.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
---
+++ Scripts/upgrade_manager.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@export var upgrades: Array[AbilityUpgrade]
